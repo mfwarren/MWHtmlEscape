a160ea0f17b13505be0f47c6b065024a453c3f8d
Matt Warren <matt.warren@gmail.com>
2014-06-12 22:58:57 -0600

    initial commit

---
+++ MWHtmlEscape/MWHtmlEscape.swift
@@ -0,0 +1,18 @@
+//
+//  MWHtmlEscape.swift
+//  MWHtmlEscape
+//
+//  Created by Matt Warren on 2014-06-12.
+//  Copyright (c) 2014 Matt Warren. All rights reserved.
+//
+
+import Foundation
+
+func escape(html: String) -> String{
+    var result = html.stringByReplacingOccurrencesOfString("&", withString: "&amp;", options: nil, range: nil)
+    result = html.stringByReplacingOccurrencesOfString("\"", withString: "&quot;", options: nil, range: nil)
+    result = html.stringByReplacingOccurrencesOfString("'", withString: "&#39;", options: nil, range: nil)
+    result = html.stringByReplacingOccurrencesOfString("<", withString: "&lt;", options: nil, range: nil)
+    result = html.stringByReplacingOccurrencesOfString(">", withString: "&gt;", options: nil, range: nil)
+    return result
+}
