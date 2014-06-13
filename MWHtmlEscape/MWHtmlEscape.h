1b952811a800c52f089a33366dcd1975e48016ff
Matt Warren <matt.warren@gmail.com>
2014-06-12 22:41:38 -0600

    Initial Commit

---
+++ MWHtmlEscape/MWHtmlEscape.h
@@ -0,0 +1,13 @@
+//
+//  MWHtmlEscape.h
+//  MWHtmlEscape
+//
+//  Created by Matt Warren on 2014-06-12.
+//  Copyright (c) 2014 Matt Warren. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MWHtmlEscape : NSObject
+
+@end
---
+++ MWHtmlEscape/MWHtmlEscape.m
@@ -0,0 +1,13 @@
+//
+//  MWHtmlEscape.m
+//  MWHtmlEscape
+//
+//  Created by Matt Warren on 2014-06-12.
+//  Copyright (c) 2014 Matt Warren. All rights reserved.
+//
+
+#import "MWHtmlEscape.h"
+
+@implementation MWHtmlEscape
+
+@end
